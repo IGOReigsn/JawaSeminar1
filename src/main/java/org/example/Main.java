7c0a2d7438ad330f3833e74c954d08c429f93ce4
IGOReigsn <eigsn@yandex.ru>
2023-06-19 13:38:06 +0300

    TaskOK_one*

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,26 +8,6 @@
         *Пункты реализовать в разных методах
 
         int i = new Random().nextInt(k); //это кидалка случайных чисел!)*/
-/*
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome! Игорь");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-           System.out.println("i = " + i);
-        }
-    }
-}
-
-*/
 //------------------------------------------------------------------------------JJHH
 package org.example;
 import java.util.Random;
@@ -35,62 +15,40 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Task1
+        System.out.println("Task1");
         int i = new Random().nextInt(2000);
-        System.out.println("1. Случайное число:"+i);
-        System.out.println("    В двоичном коде:"+Integer.toBinaryString(i));
-        Integer n= Integer.toBinaryString(i).length();
-        System.out.println("2.Номер старшего значащего бита:"+n);
-        Scanner in = new Scanner(System.in);
-        System.out.print("MAX_VALUE: ");
-        Short max_value = in.nextShort();
-        System.out.print("MIN_VALUE: ");
-        Short min_value = in.nextShort();
-        //System.out.println(max_value+"  "+min_value);
+        System.out.println(" Случайное число:" + i);
+        // Task2
+        System.out.println("Task2");
+        System.out.println("    В двоичном коде:" + Integer.toBinaryString(i));
+        Integer n = Integer.toBinaryString(i).length();
+        System.out.println("2.Номер старшего значащего бита:" + n);
+        // Task3 Задачи 3,4 сделаны с массивом, т.к. списки на момент выдачи задания "не проходили"
+        System.out.println("Task3");
+        int[] m1 = new int[(Short.MAX_VALUE - i) / n+1];
+        int index1 = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++) {
+            if (j % n == 0) {
+                m1[index1] = j;
+                index1++;
+                System.out.print(j + " ");
+            }
+        }
+        System.out.println("");
+        // Task4
+        System.out.println("Task4");
+        int tmp=(Short.MIN_VALUE - i);
+        int[] m2 = new int[tmp/n-tmp+1];//учтен знак и уменьшение количества элементов из-за отсутствия кратных чисел
+        int index2 = 0;
+        for (int j = Short.MIN_VALUE; j <= i; j++) {
+            if (j % n != 0) {
+                m2[index2] = j;
+                index2++;
+                System.out.print(j + " ");
+            }
 
-
+        }
+        System.out.println("");
     }
-/*
-        int i = 't';
-        float f = .543f;
-        long l;
-        double d;
-        short sh;
-        byte b;
-        char ch = 'g';
-        boolean b1 = false;
-
-        //---------------------------------
-
-        int[] ints = new int[12];
-        Random random = new Random();
-        for (int j = 0; j < ints.length; j++) ints[j] = random.nextInt(2000);
-
-        int[] tmp = new int[ints.length + 3];
-        System.arraycopy(ints, 0, tmp, 0, ints.length);
-        ints = tmp;
-
-        i = 0;
-        for (int j = 0, tm = 12; j < 12; j++, tm -= 23, i += j) ;
-        System.out.println(i);
-
-        if (b1) System.out.println(b1);
-
-        String str = "РџСЂРёРІРµС‚ РњРёСЂ!";
-        str += "?";
-
-        System.out.println(str.substring(1, str.length() - 1));
-        System.out.println("HI!".toLowerCase().repeat(3).length());
-
-        Scanner scanner = new Scanner(System.in);
-
-        String s;
-        do {
-            s = scanner.nextLine();
-            System.out.println(s);
-        } while (!s.equals("q"));
-
-
-    }
-*/
 }
